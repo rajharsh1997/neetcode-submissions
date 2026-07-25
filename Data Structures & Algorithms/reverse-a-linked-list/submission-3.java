fcfe58e74594abdabf78ac3f89840dceb5745521
Harsh Raj <rajharsh1997@gmail.com>
2026-07-25 22:40:22 +0530

    Add: reverse-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-3.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+  * public class ListNode {
+   *     int val;
+    *     ListNode next;
+     *     ListNode() {}
+      *     ListNode(int val) { this.val = val; }
+       *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        * }
+         */
+
+         class Solution {
+             public ListNode reverseList(ListNode head) {
+
+                     if (head == null){
+                                 return null;
+                                         }
+                                                 if (head.next == null){
+                                                             return head;
+                                                                     }
+
+                                                                             ListNode curr = head;
+                                                                                     ListNode prev = null;
+
+                                                                                             while (curr != null) {
+                                                                                                         ListNode tempNext = curr.next;
+                                                                                                                     curr.next = prev;
+                                                                                                                                 prev = curr;
+                                                                                                                                             curr = tempNext;
+                                                                                                                                                     }
+
+                                                                                                                                                             //prev has reversed L L we can return it or do head = prev
+                                                                                                                                                                     return prev;
+                                                                                                                                                                         }
+                                                                                                                                                                         }
+
+                                                                                                                                                                         
